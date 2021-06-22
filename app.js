3f74780e980d94c7d355679a9b0aed3c63ad9651
Victor Lupascu <sinajeen@gmail.com>
2021-06-21 19:21:03 -0500

    create html template using node

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const profileDataArr = process.argv.slice(2, process.argv.length);
-console.log(profileDataArr);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-printProfileData(profileDataArr);
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
